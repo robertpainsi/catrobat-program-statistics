79d5388592120848b7136fdc84e3fa70efe9ec12
Robert Painsi <robertpainsi@gmail.com>
2019-04-05 13:49:30 +0200

    Add unsupported brick BroadcastReceiverBrick

--- src/program-stats.js
+++ src/program-stats.js
@@ -28,6 +28,7 @@ const unsupportedBricks = [
     'IfLogicElseSimpleBrick',
     'IfLogicEndSimpleBrick',
     'VideoBrick',
+    'BroadcastReceiverBrick', // XXX: Ignore for now, only in 82911
 ];
 
 export async function getProgramStatsFromFile(file) {
